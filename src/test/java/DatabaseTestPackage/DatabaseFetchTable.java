4bfe09b4703a57d688d0cd0dde79f09bb471882b
Administrator <Administrator@ISD_8_C_11>
2025-03-19 17:37:04 +0530

    NewPAT

--- src/test/java/DatabaseTestPackage/DatabaseFetchTable.java
+++ src/test/java/DatabaseTestPackage/DatabaseFetchTable.java
@@ -116,4 +116,4 @@ public class DatabaseFetchTable
 	}*/
 //}
 
-//This is new changes new chnages with new data   new repo again
+//This is new changes new chnages with new data   new repo again my 
